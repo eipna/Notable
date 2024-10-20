7ea05cdc8ab93c847db98ff4714170724b05b0f7
Eipna <Xirveipna@tuta.io>
2024-10-21 00:41:48 +0800

    Read and create operation for note in database

--- app/src/main/java/com/eipna/notable/data/Database.java
+++ app/src/main/java/com/eipna/notable/data/Database.java
@@ -1,10 +1,17 @@
 package com.eipna.notable.data;
 
+import android.annotation.SuppressLint;
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 import androidx.annotation.Nullable;
+
+import com.eipna.notable.data.model.NoteModel;
+
+import java.util.ArrayList;
 
 public class Database extends SQLiteOpenHelper {
 
@@ -35,5 +42,26 @@ public class Database extends SQLiteOpenHelper {
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
         sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + TABLE_NOTE);
         onCreate(sqLiteDatabase);
+    }
+
+    public void createNote(NoteModel note) {
+        ContentValues values = new ContentValues();
+        values.put(COLUMN_NOTE_TITLE, note.getNoteTitle());
+        values.put(COLUMN_NOTE_CONTENT, note.getNoteContent());
+        getWritableDatabase().insert(TABLE_NOTE, null, values);
+        close();
+    }
+
+    public ArrayList<NoteModel> readNotes() {
+        final ArrayList<NoteModel> notes = new ArrayList<>();
+        String readQuery = "SELECT * FROM " + TABLE_NOTE;
+
+        @SuppressLint("Recycle")
+        Cursor cursor = getReadableDatabase().rawQuery(readQuery, null);
+        while (cursor.moveToNext()) {
+            notes.add(new NoteModel(cursor.getInt(0), cursor.getString(1), cursor.getString(2)));
+        }
+        cursor.close();
+        return notes;
     }
 }
