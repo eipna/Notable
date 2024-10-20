09c43cd2a05da9f7c189e6aff8e64670d63400f9
Eipna <Xirveipna@tuta.io>
2024-10-21 00:11:03 +0800

    Date creation text updates to current date in create note activity

--- app/src/main/java/com/eipna/notable/ui/activity/CreateActivity.java
+++ app/src/main/java/com/eipna/notable/ui/activity/CreateActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 
 import com.eipna.notable.R;
 import com.eipna.notable.databinding.ActivityCreateBinding;
+import com.eipna.notable.util.DateUtil;
 
 public class CreateActivity extends AppCompatActivity {
 
@@ -21,5 +22,8 @@ public class CreateActivity extends AppCompatActivity {
         if (getSupportActionBar() != null) {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         }
+
+        // Sets date creation text to current date (Eg. Monday, October 21 2024)
+        binding.dateText.setText(DateUtil.getDateString(DateUtil.PATTERN_DETAILED, DateUtil.getCurrentTime()));
     }
 }
