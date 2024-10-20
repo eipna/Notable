fa70d58387121ac0399ec84f6d0515ab3504194a
Eipna <Xirveipna@tuta.io>
2024-10-21 00:07:25 +0800

    Date util class and simple date getter

---
+++ app/src/main/java/com/eipna/notable/util/DateUtil.java
@@ -0,0 +1,21 @@
+package com.eipna.notable.util;
+
+import android.annotation.SuppressLint;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class DateUtil {
+
+    public static final String PATTERN_DETAILED = "EEEE, dd MMMM yyyy";
+
+    public static long getCurrentTime() {
+        return System.currentTimeMillis();
+    }
+
+    public static String getDateString(String pattern, long timestamp) {
+        @SuppressLint("SimpleDateFormat")
+        SimpleDateFormat dateFormat = new SimpleDateFormat(pattern);
+        return dateFormat.format(new Date(timestamp));
+    }
+}
