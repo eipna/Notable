00f1c7f63bf35b4b13cf1633ecfcb6985aaf8cfa
Eipna <Xirveipna@tuta.io>
2024-10-21 23:17:59 +0800

    Setup toolbar for update activity

--- app/src/main/java/com/eipna/notable/ui/activity/UpdateActivity.java
+++ app/src/main/java/com/eipna/notable/ui/activity/UpdateActivity.java
@@ -1,16 +1,47 @@
 package com.eipna.notable.ui.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.eipna.notable.R;
+import com.eipna.notable.databinding.ActivityUpdateBinding;
 
 public class UpdateActivity extends AppCompatActivity {
+
+    private ActivityUpdateBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_update);
+        binding = ActivityUpdateBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        setSupportActionBar(binding.toolbar);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            updateNote();
+        }
+
+        return true;
+    }
+
+    private void updateNote() {
+        closeActivity();
+    }
+
+    private void closeActivity() {
+        Intent intent = new Intent();
+        setResult(RESULT_OK, intent);
+        finish();
     }
 }
