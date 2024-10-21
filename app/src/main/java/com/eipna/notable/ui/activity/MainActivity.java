9ec59dd4ab9bab9a4a6f837e4b868ffc0cc59bdd
Eipna <Xirveipna@tuta.io>
2024-10-21 21:48:33 +0800

    Load notes in main activity

--- app/src/main/java/com/eipna/notable/ui/activity/MainActivity.java
+++ app/src/main/java/com/eipna/notable/ui/activity/MainActivity.java
@@ -3,32 +3,50 @@ package com.eipna.notable.ui.activity;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
 
+import com.eipna.notable.data.Database;
+import com.eipna.notable.data.model.NoteModel;
 import com.eipna.notable.databinding.ActivityMainBinding;
+import com.eipna.notable.ui.adapter.NoteAdapter;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
+    private Database database;
+    private ArrayList<NoteModel> notes;
+    private NoteAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-
         setSupportActionBar(binding.toolbar);
+        database = new Database(MainActivity.this);
+
+        updateNoteList();
 
         binding.createNote.setOnClickListener(view -> createNoteLauncher.launch(new Intent(MainActivity.this, CreateActivity.class)));
     }
 
     private final ActivityResultLauncher<Intent> createNoteLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), result -> {
         if (result.getResultCode() == RESULT_OK) {
-            Toast.makeText(this, "DEBUG: New Note Created", Toast.LENGTH_SHORT).show();
+            updateNoteList();
         }
     });
+
+    private void updateNoteList() {
+        notes = database.readNotes();
+        adapter = new NoteAdapter(MainActivity.this, notes);
+        binding.noteList.setLayoutManager(new LinearLayoutManager(this));
+        binding.noteList.setAdapter(adapter);
+    }
 }
