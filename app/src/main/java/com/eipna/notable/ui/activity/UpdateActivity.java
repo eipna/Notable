012834cc0f84d0b92d82720691845844174bddfa
Eipna <Xirveipna@tuta.io>
2024-10-21 23:28:06 +0800

    Apply extras to fields and text in update note activity

--- app/src/main/java/com/eipna/notable/ui/activity/UpdateActivity.java
+++ app/src/main/java/com/eipna/notable/ui/activity/UpdateActivity.java
@@ -9,10 +9,15 @@ import android.view.MenuItem;
 
 import com.eipna.notable.R;
 import com.eipna.notable.databinding.ActivityUpdateBinding;
+import com.eipna.notable.util.DateUtil;
 
 public class UpdateActivity extends AppCompatActivity {
 
     private ActivityUpdateBinding binding;
+
+    private int noteIdExtra;
+    private String noteTitleExtra, noteContentExtra;
+    private long noteDateCreatedExtra;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,10 +25,23 @@ public class UpdateActivity extends AppCompatActivity {
         binding = ActivityUpdateBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        getExtras();
+
+        binding.titleInput.setText(noteTitleExtra);
+        binding.noteInput.setText(noteContentExtra);
+        binding.dateText.setText(DateUtil.getDateString(DateUtil.PATTERN_DETAILED, noteDateCreatedExtra));
+
         setSupportActionBar(binding.toolbar);
         if (getSupportActionBar() != null) {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         }
+    }
+
+    private void getExtras() {
+        noteIdExtra = getIntent().getIntExtra("NOTE_ID", -1);
+        noteTitleExtra = getIntent().getStringExtra("NOTE_TITLE");
+        noteContentExtra = getIntent().getStringExtra("NOTE_CONTENT");
+        noteDateCreatedExtra = getIntent().getLongExtra("NOTE_DATE_CREATED", -1);
     }
 
     @Override
