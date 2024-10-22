6b165fbc16267a1a29ac2137a7bf1d3d1c8d70e3
Eipna <Xirveipna@tuta.io>
2024-10-22 20:55:03 +0800

    Toolbar and fragment setup for settings

--- app/src/main/java/com/eipna/notable/ui/activity/SettingsActivity.java
+++ app/src/main/java/com/eipna/notable/ui/activity/SettingsActivity.java
@@ -5,12 +5,27 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.eipna.notable.R;
+import com.eipna.notable.databinding.ActivitySettingsBinding;
+import com.eipna.notable.ui.fragment.SettingsFragment;
 
 public class SettingsActivity extends AppCompatActivity {
+
+    private ActivitySettingsBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_settings);
+        binding = ActivitySettingsBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        setSupportActionBar(binding.toolbar);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.settingsContainer, new SettingsFragment())
+                .commit();
     }
 }
