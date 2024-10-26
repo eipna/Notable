f7ab76ce2064a509df147d986821d749e1f62090
Eipna <Xirveipna@tuta.io>
2024-10-26 23:22:30 +0800

    Created activity for trash and archive

---
+++ app/src/main/java/com/eipna/notable/ui/activity/TrashActivity.java
@@ -0,0 +1,16 @@
+package com.eipna.notable.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.eipna.notable.R;
+
+public class TrashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trash);
+    }
+}
