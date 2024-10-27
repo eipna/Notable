af83d483a13bac5695a66819f751df4017353af5
Eipna <Xirveipna@tuta.io>
2024-10-27 13:34:32 +0800

    Send ok result on settings activity unload

--- app/src/main/java/com/eipna/notable/ui/activity/SettingsActivity.java
+++ app/src/main/java/com/eipna/notable/ui/activity/SettingsActivity.java
@@ -1,8 +1,11 @@
 package com.eipna.notable.ui.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.eipna.notable.R;
 import com.eipna.notable.databinding.ActivitySettingsBinding;
@@ -27,5 +30,15 @@ public class SettingsActivity extends AppCompatActivity {
                 .beginTransaction()
                 .replace(R.id.settingsContainer, new SettingsFragment())
                 .commit();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            Intent intent = new Intent();
+            setResult(RESULT_OK, intent);
+            finish();
+        }
+        return true;
     }
 }
