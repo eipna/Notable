aea388de336d54c3799ab9ac03785d45224f35c0
Eipna <Xirveipna@tuta.io>
2024-10-27 12:03:53 +0800

    Display all trashed notes in trash activity

--- app/src/main/java/com/eipna/notable/ui/activity/TrashActivity.java
+++ app/src/main/java/com/eipna/notable/ui/activity/TrashActivity.java
@@ -1,25 +1,75 @@
 package com.eipna.notable.ui.activity;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.eipna.notable.R;
+import com.eipna.notable.data.Database;
+import com.eipna.notable.data.interfaces.NoteListener;
+import com.eipna.notable.data.model.NoteModel;
 import com.eipna.notable.databinding.ActivityTrashBinding;
+import com.eipna.notable.ui.adapter.NoteAdapter;
 
-public class TrashActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class TrashActivity extends AppCompatActivity implements NoteListener {
 
     private ActivityTrashBinding binding;
+    private Database database;
+    private ArrayList<NoteModel> notes;
+    private NoteAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityTrashBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        database = new Database(TrashActivity.this);
+
+        updateNoteList();
 
         setSupportActionBar(binding.toolbar);
         if (getSupportActionBar() != null) {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         }
+    }
+
+    private final ActivityResultLauncher<Intent> updateNoteLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), result -> {
+        if (result.getResultCode() == RESULT_OK) {
+            updateNoteList();
+        }
+    });
+
+    private void updateNoteList() {
+        notes = database.readNotes(NoteModel.STATUS_DELETED);
+        adapter = new NoteAdapter(this, this, notes);
+        binding.noteList.setLayoutManager(new LinearLayoutManager(TrashActivity.this));
+        binding.noteList.setAdapter(adapter);
+    }
+
+    @Override
+    public void onNoteClick(int position) {
+        NoteModel note = notes.get(position);
+        int noteId = note.getNoteId();
+        String noteTitle = note.getNoteTitle();
+        String noteContent = note.getNoteContent();
+        long noteDateCreated = note.getNoteDateCreated();
+
+        Intent updateNoteIntent = new Intent(TrashActivity.this, UpdateActivity.class);
+        updateNoteIntent.putExtra("NOTE_ID", noteId);
+        updateNoteIntent.putExtra("NOTE_TITLE", noteTitle);
+        updateNoteIntent.putExtra("NOTE_CONTENT", noteContent);
+        updateNoteIntent.putExtra("NOTE_DATE_CREATED", noteDateCreated);
+        updateNoteLauncher.launch(updateNoteIntent);
+    }
+
+    @Override
+    public void onNoteLongClick(int position) {
+        // Do nothing at the moment
     }
 }
