837397b9ca29eb0bc7725b3c1f9b6f804e5a6e5c
Eipna <Xirveipna@tuta.io>
2024-10-29 00:50:53 +0800

    Libraries list preference

--- app/src/main/java/com/eipna/notable/ui/fragment/SettingsFragment.java
+++ app/src/main/java/com/eipna/notable/ui/fragment/SettingsFragment.java
@@ -1,9 +1,15 @@
 package com.eipna.notable.ui.fragment;
 
+import android.annotation.SuppressLint;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatDelegate;
 import androidx.preference.ListPreference;
 import androidx.preference.Preference;
@@ -12,11 +18,14 @@ import androidx.preference.PreferenceFragmentCompat;
 import com.eipna.notable.R;
 import com.eipna.notable.util.SharedPrefsUtil;
 
+import java.util.Objects;
+
 public class SettingsFragment extends PreferenceFragmentCompat {
 
     private ListPreference listTheme;
     private ListPreference listDisplay;
     private Preference prefsVersion;
+    private Preference prefsLibraries;
     private SharedPrefsUtil sharedPrefs;
 
     private int easterEggCounter;
@@ -46,11 +55,45 @@ public class SettingsFragment extends PreferenceFragmentCompat {
             setDisplay((String) newValue);
             return true;
         });
+
+        prefsLibraries.setOnPreferenceClickListener(preference -> {
+            showLibrariesDialog();
+            return true;
+        });
+    }
+
+    private void showLibrariesDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(requireContext())
+                .setTitle("Libraries")
+                .setItems(R.array.libraries, (dialogInterface, item) -> linkLauncher(item));
+
+        AlertDialog librariesDialog = builder.create();
+        librariesDialog.setOnShowListener(dialogInterface -> {
+            @SuppressLint("UseCompatLoadingForDrawables")
+            Drawable popupMenuBG = getResources().getDrawable(R.drawable.popup_menu, requireContext().getTheme());
+            Objects.requireNonNull(librariesDialog.getWindow()).setBackgroundDrawable(popupMenuBG);
+        });
+        librariesDialog.show();
+    }
+
+    private void linkLauncher(int item) {
+        final Intent intent = new Intent(Intent.ACTION_VIEW);
+        final String prettyTimeLink = getResources().getString(R.string.link_pretty_time);
+
+        switch (item) {
+            case 0:
+                intent.setData(Uri.parse(prettyTimeLink));
+                break;
+        }
+        startActivity(intent);
     }
 
     private void setPreferences() {
         sharedPrefs = new SharedPrefsUtil(requireContext());
+
         prefsVersion = findPreference("prefs_version");
+        prefsLibraries = findPreference("prefs_libraries");
+
         listTheme = findPreference("list_theme");
         listDisplay = findPreference("list_display");
     }
