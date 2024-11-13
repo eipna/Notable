fe44ee08e28df894571103d3aa9a6fb80baefb45
Eipna <Xirveipna@tuta.io>
2024-11-14 04:28:08 +0800

    Modified note state enum

--- app/src/main/java/com/eipna/notable/constants/NoteState.java
+++ app/src/main/java/com/eipna/notable/constants/NoteState.java
@@ -1,15 +1,29 @@
 package com.eipna.notable.constants;
 
+import androidx.annotation.NonNull;
+
 public enum NoteState {
-    ACTIVE(1), ARCHIVED(2), DELETED(3), FAVORITE_YES(1), FAVORITE_NO(0);
+    ACTIVE(1, "Active"),
+    ARCHIVED(2, "Archived"),
+    DELETED(3, "Deleted"),
+    FAVORITE_YES(1, "Favorite"),
+    FAVORITE_NO(0, "Not Favorite");
 
-    private final int state;
+    private final int value;
+    private final String string;
 
-    NoteState(int state) {
-        this.state = state;
+    NoteState(int value, String string) {
+        this.value = value;
+        this.string = string;
     }
 
-    public int getState() {
-        return state;
+    public int getValue() {
+        return value;
+    }
+
+    @NonNull
+    @Override
+    public String toString() {
+        return string;
     }
 }
