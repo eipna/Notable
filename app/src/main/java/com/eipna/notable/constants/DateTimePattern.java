f42f3b285c37c4fa61b7812abd323d644e9de6e1
Eipna <Xirveipna@tuta.io>
2024-11-14 05:01:06 +0800

    Enum for date time patterns

---
+++ app/src/main/java/com/eipna/notable/constants/DateTimePattern.java
@@ -0,0 +1,20 @@
+package com.eipna.notable.constants;
+
+public enum DateTimePattern {
+    DETAILED_WITH_TIME("MMMM dd yyyy hh:mm a"),
+    DETAILED_WITHOUT_TIME("MMMM dd, yyyy"),
+    DAY_MONTH_YEAR("dd/MM/yyyy"),
+    MONTH_DAY_YEAR("MM/dd/yyyy"),
+    YEAR_MONTH_DAY("yyyy/MM/dd"),
+    YEAR_DAY_MONTH("yyyy/dd/MM");
+
+    private final String pattern;
+
+    DateTimePattern(String pattern) {
+        this.pattern = pattern;
+    }
+
+    public String getPattern() {
+        return pattern;
+    }
+}
