cf0de2d521c549d360f589b68a421475b15d279f
Eipna <Xirveipna@tuta.io>
2024-11-22 21:35:02 +0800

    Added checked menu item getter function in note sort enum

--- app/src/main/java/com/eipna/notable/constants/NoteSort.java
+++ app/src/main/java/com/eipna/notable/constants/NoteSort.java
@@ -2,6 +2,7 @@ package com.eipna.notable.constants;
 
 import android.content.Context;
 
+import com.eipna.notable.R;
 import com.eipna.notable.models.NoteModel;
 import com.eipna.notable.utils.SharedPrefsUtil;
 
@@ -41,5 +42,23 @@ public enum NoteSort {
             return NoteModel.SORT_LAST_UPDATED_DESCENDING;
         }
         return null;
+    }
+
+    public static int getMenuItem(Context context) {
+        int prefsNoteSorting = new SharedPrefsUtil(context).getInt("prefs_note_sort", LAST_UPDATED_DESCENDING.getValue());
+        if (prefsNoteSorting == TITLE_ASCENDING.getValue()) {
+            return R.id.sort_title_asc;
+        } else if (prefsNoteSorting == TITLE_DESCENDING.getValue()) {
+            return R.id.sort_title_desc;
+        } else if (prefsNoteSorting == DATE_CREATED_ASCENDING.getValue()) {
+            return R.id.sort_date_created_asc;
+        } else if (prefsNoteSorting == DATE_CREATED_DESCENDING.getValue()) {
+            return R.id.sort_date_created_desc;
+        } else if (prefsNoteSorting == LAST_UPDATED_ASCENDING.getValue()) {
+            return R.id.sort_last_updated_asc;
+        } else if (prefsNoteSorting == LAST_UPDATED_DESCENDING.getValue()) {
+            return R.id.sort_last_updated_desc;
+        }
+        return -1;
     }
 }
