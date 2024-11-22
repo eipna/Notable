41eee8a8b99fad606e403ec37c9c302df06a91f4
Eipna <Xirveipna@tuta.io>
2024-11-22 21:00:16 +0800

    Comparator getter in note sort enum

--- app/src/main/java/com/eipna/notable/constants/NoteSort.java
+++ app/src/main/java/com/eipna/notable/constants/NoteSort.java
@@ -1,5 +1,12 @@
 package com.eipna.notable.constants;
 
+import android.content.Context;
+
+import com.eipna.notable.models.NoteModel;
+import com.eipna.notable.utils.SharedPrefsUtil;
+
+import java.util.Comparator;
+
 public enum NoteSort {
     TITLE_ASCENDING(1),
     TITLE_DESCENDING(2),
@@ -16,5 +23,23 @@ public enum NoteSort {
 
     public int getValue() {
         return value;
+    }
+
+    public Comparator<NoteModel> getComparator(Context context) {
+        int defSorting = new SharedPrefsUtil(context).getInt("prefs_note_sorting", LAST_UPDATED_ASCENDING.getValue());
+        if (defSorting == TITLE_ASCENDING.getValue()) {
+            return NoteModel.SORT_TITLE_ASCENDING;
+        } else if (defSorting == TITLE_DESCENDING.getValue()) {
+            return NoteModel.SORT_TITLE_DESCENDING;
+        } else if (defSorting == DATE_CREATED_ASCENDING.getValue()) {
+            return NoteModel.SORT_DATE_CREATED_ASCENDING;
+        } else if (defSorting == DATE_CREATED_DESCENDING.getValue()) {
+            return NoteModel.SORT_DATE_CREATED_DESCENDING;
+        } else if (defSorting == LAST_UPDATED_ASCENDING.getValue()) {
+            return NoteModel.SORT_LAST_UPDATED_ASCENDING;
+        } else if (defSorting == LAST_UPDATED_DESCENDING.getValue()) {
+            return NoteModel.SORT_LAST_UPDATED_DESCENDING;
+        }
+        return null;
     }
 }
